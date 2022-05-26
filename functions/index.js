7fbcb0cc2c2347e00f14af8caca4ed98ee25eb81
Maxime PIGEON <maxpige@gmail.com>
2022-05-26 17:35:44 +0200

    add firebase

---
+++ functions/index.js
@@ -0,0 +1,25 @@
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+admin.initializeApp()
+
+exports.addUserClaim = functions.https.onCall((data, context) => {
+  return admin
+    .auth()
+    .getUserByEmail(data.email)
+    .then((user) => {
+      return admin.auth().setCustomUserClaims(user.uid, {
+        'https://dgraph.io/jwt/claims': {
+          USER: data.email,
+          isAuthenticated: 'true',
+        },
+      })
+    })
+    .then(() => {
+      return {
+        message: `Success!`,
+      }
+    })
+    .catch((err) => {
+      return err
+    })
+})
