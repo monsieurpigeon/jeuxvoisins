1fcaf82d9454da74941c2e26d1087a97e1c7e9f8
Maxime PIGEON <maxpige@gmail.com>
2022-06-17 19:52:54 +0200

    save zip code value to user data

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,6 +1,7 @@
+import { useState } from 'react'
 import styled from 'styled-components'
 import { useAuth } from '../contextes/auth'
-import { useGetUserQuery } from '../generated/graphql'
+import { useGetUserQuery, useUpdateUserMutation } from '../generated/graphql'
 
 type Props = {}
 
@@ -15,11 +16,35 @@ const Profile: React.FC<Props> = () => {
   const { data } = useGetUserQuery({
     variables: { email: currentUser?.email || '' },
   })
+  const [zipCode, setZipCode] = useState('')
+
+  const [update] = useUpdateUserMutation()
+
   return (
     <Container>
       <h1>Profil</h1>
       <div>username: {data?.getUser?.username}</div>
       <div>email: {data?.getUser?.email}</div>
+      <div>code postal: {data?.getUser?.zipCode}</div>
+      <input
+        type="text"
+        value={zipCode}
+        onChange={(e) => setZipCode(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          update({
+            variables: {
+              patch: {
+                filter: { username: { eq: data?.getUser?.username || '' } },
+                set: { zipCode: zipCode },
+              },
+            },
+          })
+        }}
+      >
+        Enregistrer le code postal
+      </button>
     </Container>
   )
 }
